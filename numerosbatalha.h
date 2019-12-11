c4e97ecb122b44a521a263c8c2046e787a284845
IloFelipeUnipe <55170305+IloFelipeUnipe@users.noreply.github.com>
2019-12-11 01:41:55 -0300

    Update numerosbatalha.h

--- numerosbatalha.h
+++ numerosbatalha.h
@@ -1,16 +1,74 @@
-#ifndef numerosbatalha_h   // guardas de cabeçalho, impedem inclusões cíclicas
+#ifndef numerosbatalha_h  
 #define numerosbatalha_h
 
 #include<stdio.h>
 #include<conio.h>
 
-int multiplica(int forca, int defesa) //multiplica recebe N1,N2 e retorna um int
+int ataque(int forca, int mdefesa)
 {
-  int resultado;
-  resultado = forca * defesa;
-  return(resultado); //retornando o valor para main
+  int dano;
+  dano = (forca/2) - (mdefesa/2);
+  if(dano <= 0){
+  	dano = 1;
+  }if(dano >= 20){
+  	dano = 20;
+  }
+  return(dano); 
 }
 
+int ataquemagico(int inteligencia,int espirito ,int mdefesa ,int mespirito)
+{
+  int dano;
+  dano = (inteligencia + (espirito/4)) - (mdefesa + (mespirito/4));
+  if (dano <= 0)
+  {
+  	dano = 1;
+  }if(dano >= 20)
+  {
+  	dano = 20;
+  }
+  return(dano);
+  
+  
+}
 
+int efeitocura(int espirito, int inteligencia)
+{
+	int cura;
+	cura = ((espirito/3) + (inteligencia/4));
+	if(cura <= 0)
+	{
+		cura = 1;
+	}
+	return(cura);	
+}
+
+int danoinimigofisico(int mforca, int defesa)
+{
+	int mdano;
+	mdano = mforca - defesa;
+	if(mdano <= 0)
+	{
+		mdano = 1;
+		
+	}if(mdano >= 20){
+  	mdano = 20;
+  	}
+	return(mdano);	
+}
+
+int danoinimigomagico(int minteligencia , int mespirito , int defesa , int espirito)
+{
+	int mdano;
+	mdano = ((minteligencia/2) + (mespirito/4)) - (defesa + (espirito/4)); 
+	if (mdano <= 0){
+  	mdano = 1;
+ 	}
+	if(mdano >= 20){
+  	mdano = 20;
+  	}
+  return(mdano);
+	
+}
 
 #endif
